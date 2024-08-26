eae775905855ef5f29cfc0a4baa8bab60775dfb8
Ycidia <lloydseth16@gmail.com>
2024-08-26 14:32:41 +0100

    Non-constant variable

--- source/matrix.hpp
+++ source/matrix.hpp
@@ -8,7 +8,7 @@ namespace cpp_utils {
 
 class Matrix{
     public:
-        const double xx, xy, xz, yx, yy, yz, zx, zy, zz;
+        double xx, xy, xz, yx, yy, yz, zx, zy, zz;
         inline Matrix() : xx(1), xy(0), xz(0), yx(0), yy(1), yz(0), zx(0), zy(0), zz(1) {};
         Matrix(double xx, double xy, double xz, double yx, double yy, double yz, double zx, double zy, double zz);
         Matrix(double xx, double yy, double zz);
